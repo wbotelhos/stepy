2e6cc19bbb47d16b8de9fbcd48170bb7200ba39a
washingtonbotelho <wbotelhos@gmail.com>
2010-11-22 22:43:13 -0200

    Rewrited the whole code for a better plugin. (:

--- js/jquery.stepy.js
+++ js/jquery.stepy.js
@@ -1,268 +1,290 @@
 /**
  * jQuery Stepy - A Wizard Plugin - http://wbotelhos.com/stepy
- * --------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------------
  *
  * jQuery Stepy is a plugin based on FormToWizard that generates a customizable wizard.
  * 
  * Licensed under The MIT License
  * 
- * @version     0.1
- * @since       07.03.2010
- * @author      Washington Botelho dos Santos
- * @link        http://wbotelhos.com/stepy
- * @twitter     http://twitter.com/wbotelhos
- * @license     http://www.opensource.org/licenses/mit-license.php MIT 
- * @package     jQuery Plugins
+ * @version			0.2
+ * @since			07.03.2010
+ * @author			Washington Botelho dos Santos
+ * @documentation	http://wbotelhos.com/raty
+ * @twitter			http://twitter.com/wbotelhos
+ * @license			http://opensource.org/licenses/mit-license.php MIT
+ * @package			jQuery Plugins
  * 
- * Usage (default values):
- * --------------------------------------------------------------------------
- *  $('#default').stepy({
- *      backLabel:        '&lt; Back',  // Change the back button label.
- *      includeFinish:    true,         // Include the button with class called '.finish' into the last step.
- *      nextLabel:        'Next &gt;',  // Change the next button label.
- *      titleClick:       true,         // Active the back and next action in the titles.
- *      validate:         false         // Active the jQuery Validation. Depends of jquery.validation.js. A div with class 'error-step' is created automatically to put the errors.
- *  });
+ * Usage with default values:
+ * ---------------------------------------------------------------------------------
+ * $('#default').stepy();
  *  
- *  <form id="default">
- *      <fieldset title="Step 1">
- *          <legend>description one</legend>
- *          <!-- input fields -->
- *      </fieldset>
+ *	<form id="default">
+ *		<fieldset title="Step 1">
+ *			<legend>description one</legend>
+ *			<!-- input fields -->
+ *		</fieldset>
  *
- *      <fieldset title="Step 2">
- *          <legend>description one</legend>
- *          <!-- input fields -->
- *      </fieldset>
+ *		<fieldset title="Step 2">
+ *			<legend>description one</legend>
+ *			<!-- input fields -->
+ *		</fieldset>
  *
- *      // and so on..
+ *		// and so on..
  *
- *      <input type="submit" class="finish" value="Finish!"/>
- *  </form>
- *
- * Public functions:
- * --------------------------------------------------------------------------
- *
- *  $.fn.stepy.step(2, 'default');  // Go to step 2 with ID called 'default' later. If validation is enabled this feature should come after the call of validate.
+ *		<input type="submit" class="finish" value="Finish!"/>
+ *	</form>
  *
  */
 
-(function($) {
+;(function($) {
 
 	$.fn.stepy = function(settings) {
 		options = $.extend({}, $.fn.stepy.defaults, settings);
 
-		if (this.attr('id') === undefined) {
-			debug('Invalid selector!'); return;
+		if (this.length == 0) {
+			debug('Invalid selector!');
+			return;
+		} else if (this.length > 1) {
+			return this.each(function() {
+				$.fn.stepy.apply($(this), [settings]);
+			});
 		}
 
-		$this = $(this);
+		$global = $(this);
 
-		var id = $this.attr('id'),
-			steps = $this.find('fieldset'),
-			count = steps.size(),
-			isValidate = options.validate,
-			title = '',
-			description = '';
+		var id			= $global.attr('id'),
+			steps		= $global.children('fieldset'),
+			size		= steps.size(),
+			description	= '',
+			title		= '',
+			step;
 
-        $this.before('<ul id="' + id + '-titles" class="titles"></ul>');
+        var $titles = $('<ul class="stepy-titles"></ul>').insertBefore($global);
 
-        if (isForm(id) && isValidate) {
-        	$this.append('<div class="error-step"></div>');
+        if (options.validate && $global.is('form')) {
+        	$global.append('<div class="stepy-error"></div>');
         }
 
-        steps.each(function(i) {
-        	$(this).attr('id', id + '-step-' + i).append('<p id="' + id + '-buttons-' + i + '"></p>').addClass('step');
 
-        	description = $(this).children('legend:first').html();
-        	title = ($(this).attr('title') != '') ? $(this).attr('title') : '--';
-        	$('ul#' + id + '-titles').append('<li id="' + id + '-title-' + i + '">' + title  + '<span>' + description + '</span></li>');
+        steps.each(function(i) { // fieldset
+        	step = $(this);
+
+        	step
+        	.attr('id', id + '-step-' + i)
+        	.addClass('step')
+        	.append('<p id="' + id + '-buttons-' + i + '"></p>');
+
+        	title = (step.attr('title') != '') ? step.attr('title') : '--';
+
+        	description = step.children('legend:first').html();
+
+        	$titles.append('<li id="' + id + '-title-' + i + '">' + title  + '<span>' + description + '</span></li>');
 
         	if (i == 0) {
         		createNextButton(i);
-        	} else if (i == (count - 1)) {
-        		createBackButton(i);
-        		$('fieldset#' + id + '-step-' + i).hide();
         	} else {
         		createBackButton(i);
-        		createNextButton(i);
-        		$('fieldset#' + id + '-step-' + i).hide();
+        		step.hide();
+
+        		if (i != size - 1) {
+	        		createNextButton(i);
+	        	}
         	}
         });
-        
-        $('li#' + id + '-title-0').addClass('current-step');
 
-        var finish = $('#' + id + ' .finish');
-        
-        if (finish[0]) {
-        	if (options.includeFinish) {
-        		finish = finish.removeClass('finish').addClass('button-finish').hide().appendTo('p#' + id + '-buttons-' + (count - 1));
-        	}
-        } else {
-        	if (isForm(id)) {
-        		debug('You should create a button with a class named "finish" when the attribute includeFinish is true.');
-        	}
+        $titles.children('li:first').addClass('current-step');
+
+        var finish = $global.children('.finish');
+
+        if (options.finish) {
+	        if (finish.length) {
+        		finish.hide().appendTo($global.find('p:last'));
+	        } else if ($global.is('form')) {
+	        	debug('You should create a button with a class named "finish" when the attribute \'finish\' is true.');
+	        }
         }
 
-        function getCurrentStepId(id) {
-			var vet = $('ul#' + id + '-titles').find('li.current-step').attr('id').split('-');
-			return parseInt(vet[vet.length - 1]);
-        }
-
-        function getClickedStepId(title) {
-        	var vet = title.attr('id').split('-');
-			return parseInt(vet[vet.length - 1]);
-        }
+        var $this	= $global,
+        	opt		= options;
 
         if (options.titleClick) {
-			$('li', $('ul#' + id + '-titles')[0]).live('click', function() {
-				var idCurrent = getCurrentStepId(id), idClicked = getClickedStepId($(this));
+        	$titles.children().click(function() {
+        		var clicked = parseInt($(this).attr('id').match(/\d/)),
+        			current = parseInt($titles.children('.current-step').attr('id').match(/\d/)),
+        			maxStep = clicked;
 
-				if (idClicked == idCurrent) {
-					return false;
-				}
+				if (clicked > current) {									// Validate only clickeds steps ahead.
+					var isValid = true;
 
-				var maxStep = idClicked, isValid = true;
+					if (opt.validate) {
+						for (var i = 0; i < clicked; i++) {
+							isValid = validate($this, i) && isValid;		// Accumulates validations.
 
-				if (isValidate) {
-					$('fieldset.step', $('#' + id)[0]).each(function(index) {
-						if (index >= idClicked) {
-							return false;
-						}
-						
-						if (idClicked > idCurrent) {									// Validate only clickeds steps ahead.
-							isValid = validate(id, index) && isValid;					// Accumulates validations.
-
-							if (!isValid) {												// In the first invalid step the function stops.
-								maxStep = index;										// The first invalid step.
-								return false;
+							if (!isValid) {									// In the first invalid step the function stops.
+								maxStep = i;								// The first invalid step.
+								break;
 							}
 						}
-					});
+					}
 				}
 
-				if (maxStep != idCurrent) {												// Avoid change to the same step.
-					selectStep(id, maxStep);
+				if (clicked != current) {									// Avoid change to the same step.
+					selectStep($this, maxStep);
 	
-			        if (maxStep + 1 == count) {
+			        if (opt.finish && maxStep + 1 == size) {
 	                	finish.show();
 	                }
 				}
         	});
+    	} else {
+    		$titles.children().css('cursor', 'default');
     	}
 
         function createBackButton(i) {
-        	var buttonId = id + '-back-' + i;
-            $('p#' + id + '-buttons-' + i).append('<a id="' + buttonId + '" href="javascript:void(0);" class="button-back">' + options.backLabel + '</a>');
-
-            $('a#' + buttonId).live('click', function() {
-                selectStep(id, i - 1);
+        	$('<a/>', {
+        		id:			id + '-back-' + i,
+        		href:		'javascript:void(0);',
+        		'class':	'button-back',
+        		html:		options.backLabel
+        	})
+        	.click(function() {
+                selectStep($this, i - 1);
                 finish.hide();
-            });
-        }
+            })
+        	.appendTo($('p#' + id + '-buttons-' + i));
+
+        };
 
         function createNextButton(i) {
-        	var buttonId = id + '-next-' + i;
-            $('p#' + id + '-buttons-' + i).append('<a id="' + buttonId + '" href="javascript:void(0);" class="button-next">' + options.nextLabel + '</a>');
-            $('a#' + buttonId).live('click', function() {
-            	if (!isValidate || validate(id, i)) {
-	                selectStep(id, i + 1);
-	                
-	                if (i + 2 == count) {
+        	$('<a/>', {
+        		id:			id + '-next-' + i,
+        		href:		'javascript:void(0);',
+        		'class':	'button-next',
+        		html:		options.nextLabel
+        	})
+        	.click(function() {
+            	if (!opt.validate || validate($this, i)) {
+	                selectStep($this, i + 1);
+
+	                if (i + 2 == size) {
 	                	finish.show();
 	                }
             	}
-            });
-        }
+            })
+            .appendTo($('p#' + id + '-buttons-' + i));
+        };
 
-		return $this;
+		return $global;
 	};
 	
 	$.fn.stepy.defaults = {
-		backLabel:			'&lt; Back',
-		includeFinish:		true,
-		nextLabel:			'Next &gt;',
-		titleClick:			true,
-		validate:			false
+		backLabel:		'&lt; Back',
+		finish:			true,
+		nextLabel:		'Next &gt;',
+		titleClick:		false,
+		validate:		false
 	};
 
 	$.fn.stepy.step = function(index, id) {
-		$('p#' + getPublicId(id) + '-buttons-' + (index - 2) + ' a.button-next').trigger('click');
+		selectStep(getContext(index, id, 'step'), index - 1);
+		$.fn.stepy;
 	};
-	
-	function getPublicId(id) {
+
+	function debug(message) {
+		if (console && console.log) {
+			console.log(message);
+		}
+	};
+
+	function getContext(value, id, name) {
+		var context = $global;
+
 		if (id) {
-			if ($('#' + id)[0]) {
-				return id;
-			} else {
-				debug('Invalid selector of the public funtion $.fn.stepy.step()!');
+			if (id.indexOf('.') == 0) {
+				var idEach;
+
+				return $(id).each(function() {
+					idEach = '#' + $(this).attr('id');
+
+					if (name == 'step') {
+						$.fn.raty.step(value, idEach);
+					}
+				});
 			}
-		} else {
-			return $this.attr('id');
+
+			context	= $(id);
+
+			if (!context.length) {
+				debug('"' + id + '" is a invalid ID for the public funtion $.fn.stepy.' + name + '().');
+				return;
+			}
 		}
+
+		return context;
 	};
 
-	function selectStep(id, index) {
-		var count = $('#' + id + ' fieldset').size();
+	function selectStep(context, index) {
+		var id		= context.attr('id'),
+			size	= context.children('fieldset').size(),
+			step;
 
-		if (index > count) {
-			index = count - 1;
+		if (index > size - 1) {
+			index = size - 1;
 		}
 
-		$('#' + id + ' fieldset.step').hide();
-		$('#' + id + ' fieldset#' + id + '-step-' + index).show();
+		context
+			.children('fieldset').hide()
+		.end()
+			.children('fieldset')
+				.eq(index).show();
 
-		$('ul#' + id + '-titles li').removeClass('current-step');
-        $('li#' + id + '-title-' + index).addClass('current-step');
+		context
+			.prev()
+				.children().removeClass('current-step')
+			.end()
+			.children()
+				.eq(index).addClass('current-step');
 
-        if (isForm(id)) {
-	        $(':input:visible', $('#' + id + ' fieldset#' + id + '-step-' + index)[0]).each(function() {
-	        	if (!$(this).attr('disabled')) {
-	    			$(this).focus();
-	    			return false;
-	        	}
-	        });
+        if (context.is('form')) {
+        	context
+        		.children('fieldset')
+	        		.eq(index)
+		        		.find(':input:visible')
+			        		.each(function() {
+			        			step = $(this);
+			
+					        	if (!step.attr('disabled')) {
+					    			step.focus();
+					    			return false;
+					        	}
+			        		});
         }
 	};
 
-	function isForm(id) {
-		var tipo = $('#' + id).get().toString();
-    	if (tipo === '[object HTMLFormElement]') {
-    		return true;
-    	}
-    	return false;
-	};
-
-    function validate(id, index) {
-    	if (!isForm(id)) {
+    function validate(context, index) {
+    	if (!context.is('form')) {
     		return true;
     	}
 
-    	var isValid = true, container = $('#' + id);
+    	var id		= context.attr('id'),
+    		isValid	= true,
+    		step	= context.children('fieldset').eq(index);
 
-    	$('#' + id + ' div.error-step').html('');
-    	$(':input', $('#' + id + ' fieldset#' + id + '-step-' + index)[0]).each(function() {
-    		isValid = isValid && container.validate().element($(this));
+    	context.children('.stepy-error').empty();
+
+    	step.find(':input').each(function() {
+    		isValid = isValid && context.validate().element($(this));
 
     		if (isValid === undefined) {
     			isValid = true;
     		}
 
-    		if (isValid || isValid === undefined) {
-    			selectStep(id, index);
-    		} else {
-    			container.validate().focusInvalid();
-    			isValid = false;
+    		if (!isValid) {
+    			context.validate().focusInvalid();
     		}
     	});
 
     	return isValid;
     };
     
-	function debug(message) {
-		if (window.console && window.console.log) {
-			window.console.log(message);
-		}
-	};
-
 })(jQuery);
