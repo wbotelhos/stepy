88da645977b344132fe17eed00f72f883e9e841e
Washington Botelho <wbotelhos@gmail.com>
2017-06-25 18:57:33 -0300

    no chrome spec for now

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     autoWatch  : true,
     debug: true,
-    browsers   : ['Chrome', 'Firefox'],
+    browsers   : ['Firefox'],
     files      : ['../vendor/jquery.min.js', '../vendor/jquery.validate.min.js', '../lib/*.css', '../lib/*.js', 'lib/jasmine-jquery.js', 'lib/jquery.factory.js', '*_spec.js' ],
     frameworks : ['jasmine'],
     logLevel   : config.LOG_ERROR,
