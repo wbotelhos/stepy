1065f59ee9fcaae09e945311500809e04063fc5c
Washington Botelho <wbotelhos@gmail.com>
2017-06-27 19:33:50 -0300

    fix karma load path

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -5,11 +5,20 @@ module.exports = function(config) {
     autoWatch:  true,
     debug:      true,
     browsers:   ['Firefox'],
-    files:      ['../vendor/jquery.min.js', '../vendor/jquery.validate.min.js', '../lib/*.css', '../lib/*.js', 'lib/jasmine-jquery.js', 'lib/jquery.factory.js', '*_spec.js'],
     frameworks: ['jasmine'],
     logLevel:   config.LOG_ERROR,
     port:       9876,
     reporters:  ['dots'],
-    singleRun:  true
+    singleRun:  true,
+
+    files: [
+      '../vendor/jquery.min.js',
+      '../vendor/jquery.validate.min.js',
+      '../lib/*.css',
+      '../lib/*.js',
+      'lib/jasmine-jquery.js',
+      'lib/jquery.factory.js',
+      '**/*_spec.js'
+    ]
   });
 };
