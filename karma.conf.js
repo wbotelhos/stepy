52285245e11206a5fe28e15f023784c0c1ca83fd
Washington Botelho <wbotelhos@gmail.com>
2017-06-28 14:31:46 -0300

    move karma to root with fixture module enabled

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(config) {
+  'use strict';
+
+  config.set({
+    autoWatch:  true,
+    debug:      true,
+    browsers:   ['Firefox'],
+    frameworks: ['jasmine', 'fixture'],
+    logLevel:   config.LOG_ERROR,
+    port:       9876,
+    reporters:  ['dots'],
+    singleRun:  true,
+
+    files: [
+      'vendor/jquery.min.js',
+      'vendor/jquery.validate.min.js',
+
+      'lib/*.css',
+      'lib/*.js',
+
+      'spec/fixtures/*.html',
+
+      'spec/lib/jasmine-jquery.js',
+      'spec/spec_helper.js',
+
+      'spec/javascripts/**/*.js'
+    ],
+
+    preprocessors: {
+      '**/*.html': ['html2js']
+    },
+  });
+};
