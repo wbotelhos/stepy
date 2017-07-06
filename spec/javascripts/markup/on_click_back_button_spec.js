0381768bd1259af0920ed50bbb08b7d20b1a379d
Washington Botelho <wbotelhos@gmail.com>
2017-07-06 15:30:11 -0300

    block the navigation over steps non existent

--- spec/javascripts/markup/on_click_back_button_spec.js
+++ spec/javascripts/markup/on_click_back_button_spec.js
@@ -10,12 +10,12 @@ describe('on_click_back_button', function() {
     var self = $('form').stepy().stepy('step', 1);
 
     // when
-    self.find('fieldset:eq(1) .stepy-back').trigger('click');
+    self.find('.stepy-step:eq(1) .stepy-back').trigger('click');
 
     // then
-    expect(self.find('fieldset:eq(0)')).toBeVisible();
-    expect(self.find('fieldset:eq(1)')).toBeHidden();
-    expect(self.find('fieldset:eq(2)')).toBeHidden();
+    expect(self.find('.stepy-step:eq(0)')).toHaveClass('stepy-active');
+    expect(self.find('.stepy-step:eq(1)')).not.toHaveClass('stepy-active');
+    expect(self.find('.stepy-step:eq(2)')).not.toHaveClass('stepy-active');
   });
 
   it ('focus the first field', function() {
@@ -23,9 +23,26 @@ describe('on_click_back_button', function() {
     var self = $('form').stepy().stepy('step', 1);
 
     // when
-    self.find('fieldset:eq(1) .stepy-back').trigger('click');
+    self.find('.stepy-step:eq(1) .stepy-back').trigger('click');
 
     // then
-    expect(self.find('fieldset:eq(0) :input:enabled:visible:first')).toBeFocused();
+    expect(self.find('.stepy-step:eq(0) :input:enabled:visible:first')).toBeFocused();
+  });
+
+  context('on the first step', function() {
+    it ('does not move to a non existent one', function() {
+      // given
+      var self = $('form').stepy();
+
+      // when
+      self.find('.stepy-step:eq(0) .stepy-back').trigger('click');
+
+      // then
+      expect(self.data('index')).toEqual(0);
+
+      expect(self.find('.stepy-step:eq(0)')).toHaveClass('stepy-active');
+      expect(self.find('.stepy-step:eq(1)')).not.toHaveClass('stepy-active');
+      expect(self.find('.stepy-step:eq(2)')).not.toHaveClass('stepy-active');;
+    });
   });
 });
--- spec/javascripts/markup/on_click_next_button_spec.js
+++ spec/javascripts/markup/on_click_next_button_spec.js
@@ -10,12 +10,12 @@ describe('on_click_next_button', function() {
     var self = $('form').stepy();
 
     // when
-    self.find('fieldset:eq(0) .stepy-next').click();
+    self.find('.stepy-step:eq(0) .stepy-next').trigger('click');
 
     // then
-    expect(self.find('fieldset:eq(0)')).toBeHidden();
-    expect(self.find('fieldset:eq(1)')).toBeVisible();
-    expect(self.find('fieldset:eq(2)')).toBeHidden();
+    expect(self.find('.stepy-step:eq(0)')).not.toHaveClass('stepy-active');
+    expect(self.find('.stepy-step:eq(1)')).toHaveClass('stepy-active');;
+    expect(self.find('.stepy-step:eq(2)')).not.toHaveClass('stepy-active');
   });
 
   it ('focus the first field', function() {
@@ -23,9 +23,29 @@ describe('on_click_next_button', function() {
     var self = $('form').stepy();
 
     // when
-    self.find('fieldset:eq(0) .stepy-next').click();
+    self.find('.stepy-step:eq(0) .stepy-next').trigger('click');
 
     // then
-    expect(self.find('fieldset:eq(1) :input:enabled:visible:first')).toBeFocused();
+    expect(self.find('.stepy-step:eq(1) :input:enabled:visible:first')).toBeFocused();
+  });
+
+  context('on the last step', function() {
+    it ('does not move to a non existent one', function() {
+      // given
+      var self = $('form').stepy();
+
+      self.find('.stepy-step:eq(0) .stepy-next').trigger('click');
+      self.find('.stepy-step:eq(1) .stepy-next').trigger('click');
+
+      // when
+      self.find('.stepy-step:eq(2) .stepy-next').trigger('click');
+
+      // then
+      expect(self.data('index')).toEqual(2);
+
+      expect(self.find('.stepy-step:eq(0)')).not.toHaveClass('stepy-active');
+      expect(self.find('.stepy-step:eq(1)')).not.toHaveClass('stepy-active');
+      expect(self.find('.stepy-step:eq(2)')).toHaveClass('stepy-active');;
+    });
   });
 });
