b92003dc766c26d3e8d1ae1800f567e0b1efb53f
Washington Botelho <wbotelhos@gmail.com>
2017-07-07 21:07:02 -0300

    add validaty example

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,9 @@ module.exports = function(config) {
       'node_modules/jquery/dist/jquery.min.js',
       'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
       'node_modules/jquery-validation/dist/jquery.validate.min.js',
+      'node_modules/jquery-validation/dist/jquery.validate.min.js',
+      'node_modules/validaty/lib/jquery.validaty.js',
+      'node_modules/validaty/lib/jquery.validaty.validators.js',
 
       'spec/spec_helper.js',
 
